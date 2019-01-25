cb31665f50d07bb5f87fb410bebd311107b5e249
Steffen Becker <steffen.becker@informatik.uni-stuttgart.de>
2019-01-25 19:34:55 +0100

    Fixed code for example test

--- test/de/unistuttgart/iste/rss/pse/exercise13/TestHamster.java
+++ test/de/unistuttgart/iste/rss/pse/exercise13/TestHamster.java
@@ -1,6 +1,8 @@
 package de.unistuttgart.iste.rss.pse.exercise13;
+
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Direction;
@@ -16,29 +18,35 @@ public class TestHamster {
      * Size used in this test for both, colums and rows.
      */
     private static final int TERRITORY_SIZE = 4;
+    private HamsterGame game;
 
+    @Before
+    public void init() {
+        game = new HamsterGame();
+
+        final TerritoryBuilder territoryBuilder = game.getNewTerritoryBuilder();
+        territoryBuilder.initializeTerritory(new Size(TERRITORY_SIZE, TERRITORY_SIZE));
+        /* Configure hamster with position (0,0), direction, and number of grains */
+        territoryBuilder.defaultHamsterAt(Location.ORIGIN, Direction.EAST, 0);
+
+        /* Place a grain at position (1,0) */
+        territoryBuilder.grainAt(Location.from(1, 0));
+
+        game.initialize(territoryBuilder);
+        game.startGame(false);
+    }
+    
     /**
      * Test creating a territory via API.
      */
     @Test
     public void testConfiguredHamsterOnTerritory() {
-    	HamsterGame game = new HamsterGame();
-    	Hamster paule = game.getTerritory().getDefaultHamster();
-    	
-        final TerritoryBuilder territoryBuilder = game.getNewTerritoryBuilder();
-        territoryBuilder.initializeTerritory(new Size(TERRITORY_SIZE, TERRITORY_SIZE));
-        /* Configure hamster with position (0,0), direction, and number of grains */ 
-        territoryBuilder.defaultHamsterAt(Location.ORIGIN, Direction.EAST, 0); 
-        
-        /* Place a grain at position (1,0) */ 
-        territoryBuilder.grainAt(Location.from(1, 0)); 
-
-        game.initialize(territoryBuilder);
-
-        paule = game.getTerritory().getDefaultHamster();
-        assertEquals(game.getTerritory().getTerritorySize(), new Size(TERRITORY_SIZE, TERRITORY_SIZE));
-        assertEquals(paule.getLocation(), Location.from(0, 0));
-        assertEquals(game.getTerritory().getTotalGrainCount(), 0);
+        game.runGame(territory -> {
+            final Hamster paule = territory.getDefaultHamster();
+            assertEquals(territory.getTerritorySize(), new Size(TERRITORY_SIZE, TERRITORY_SIZE));
+            assertEquals(Location.ORIGIN, paule.getLocation());
+            assertEquals(1, territory.getTotalGrainCount());
+        });
     }
 
 }
